297a6c1a2e1bea726fb0cb55956c5fd56d669967
Jonathan Aldas <jonaaldas@gmail.com>
2025-02-09 19:35:37 +0100

    test-change

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,8 @@ app.get("/", (c) => {
   return c.text("Hello Hono Cloudflare!");
 });
 
+app.get("/api/hey", (c) => {
+  return c.json({ message: "Hello from Hono Cloudflare!" });
+});
+
 export default app;
